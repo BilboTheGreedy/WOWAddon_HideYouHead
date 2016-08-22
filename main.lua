cc0cb4be2cee75dd313fa0fc500a0e6db1d474f1
BilboTheGreedy <daniel.rapp@live.se>
2016-08-22 02:19:44 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,9 +6,11 @@ HUH_SavedVars = {
 local HUHFrame = CreateFrame("Frame")
 
 function HUHFrame:OnEvent(event, addon)
+
 		if addon == "ADDON_LOADED" and HUH_SavedVars.auto then
 		HUH_SavedVars.toggle = false
 		end	
+		
 		if addon == "Blizzard_TalkingHeadUI" then
 			hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
 			if HUH_SavedVars.auto then 
@@ -18,15 +20,15 @@ function HUHFrame:OnEvent(event, addon)
 					else 
 						print("Hide You Head!")
 					end
-		     else
-				if HUH_SavedVars.toggle then
-						TalkingHeadFrame:Show()
-					else 
-						print("Hide You Head!")
-						TalkingHeadFrame:Hide()
-					end
-			  end
-			end)
+		else
+			if HUH_SavedVars.toggle then
+					TalkingHeadFrame:Show()
+				else 
+					print("Hide You Head!")
+					TalkingHeadFrame:Hide()
+				end
+			end
+		end)
 			self:UnregisterEvent(event)
 	end
 end
@@ -55,11 +57,22 @@ local function handler(msg, editbox)
 		  print("Auto Hide Persistent true")
 end
  else
-      print(GetAddOnMetadata("HideYouHead", "Title"), 'version' .. GetAddOnMetadata("HideYouHead", "Version")
+      print(GetAddOnMetadata("HideYouHead", "Title"), 'version ' .. GetAddOnMetadata("HideYouHead", "Version")
 	,"\n",
-	"/huh toggle - Hide/Show Talking Head Frame (temporairly for this session)"
+	"/huh toggle - Hide/Show Talking Head Frame"
 	,"\n",
-	"/huh auto - Sets hide/show Persistent");
+	"/huh auto - if set to true then toggle will be set to hidden when you login");
+	if HUH_SavedVars.toggle then
+	DEFAULT_CHAT_FRAME:AddMessage("Talking Head: \124cff00ff00\124hVisible\124h\124r");
+	else
+	DEFAULT_CHAT_FRAME:AddMessage("Talking Head: \124cffff0000\124hHidden\124h\124r");
+	end
+	if HUH_SavedVars.auto then
+	DEFAULT_CHAT_FRAME:AddMessage("Auto Hide at Logon: \124cff00ff00\124htrue\124h\124r");
+	else
+	DEFAULT_CHAT_FRAME:AddMessage("Auto Hide at Logon: \124cffff0000\124hfalse\124h\124r");
+	end
+
  end
 end
 SlashCmdList["HUH"] = handler;
